16e9129cc3f86256c860cc30643803c2f041c8f4
vmt-jvelez <vmt-jvelez@bancoguayaquil.com>
2022-11-11 16:14:53 -0500

    Estoy viendo la clase 27 seccion 2 voy casi al inicio

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,26 +1,6 @@
-(function () {
-    class Avenger {
-        // nombre: string;
-        // equipo: string;
-        // nombreReal: string;
-        // puedePelear: boolean;
-        // peleasGanadas: number;
-        constructor(public nombre: string,
-            public equipo?: string,
-            public nombreReal?: string,
-            public puedePelear: boolean = true,
-            public peleasGanadas: number = 0) {
-            // this.nombre = nombre;
-            // this.equipo = equipo;
-            // this.nombreReal = nombreReal;
-            // this.peleasGanadas = peleasGanadas;
-            // this.puedePelear = puedePelear;
-        }
+(() => {
+    let sumar = (a: number, b: number) => {
+        return a + b;
     }
-
-    let antman = new Avenger('Antman', 'Capi');
-    
-    console.log(antman);
-
 
 })();
