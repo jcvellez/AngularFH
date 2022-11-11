9d0c933d351bd5a5b8d64225819a3dc6fbc8a6dd
vmt-jvelez <vmt-jvelez@bancoguayaquil.com>
2022-11-11 16:22:05 -0500

    Estoy en el video 27 seccion 2

---
+++ typescript-importer/src/classes/xmen.class.ts
@@ -0,0 +1,18 @@
+function imprimirConsola(constructorClase:Function) {
+    console.log(constructorClase);
+    
+}
+
+@imprimirConsola
+export class Xmen {
+    constructor(
+        public nombre: string,
+        public clave : string
+    ) { }
+
+imprimir(){
+    console.log(`${this.nombre} - ${this.clave}`);
+    
+}
+
+}
---
+++ typescript-importer/src/index.ts
@@ -0,0 +1,6 @@
+import {Xmen } from "./classes/xmen.class";
+
+let wolverine= new Xmen('Logan','Wolverine');
+
+wolverine.imprimir();
+
