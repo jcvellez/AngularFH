97b291bf8aafe02ccd68cfea31b29521f3b58f3b
jcvellez <jcvellez@live.com>
2022-11-12 13:48:35 -0500

    cambios en app

--- typescript/app.js
+++ typescript/app.js
@@ -1,24 +1,6 @@
 "use strict";
-(function () {
-    class Avenger {
-        // nombre: string;
-        // equipo: string;
-        // nombreReal: string;
-        // puedePelear: boolean;
-        // peleasGanadas: number;
-        constructor(nombre, equipo, nombreReal, puedePelear = true, peleasGanadas = 0) {
-            this.nombre = nombre;
-            this.equipo = equipo;
-            this.nombreReal = nombreReal;
-            this.puedePelear = puedePelear;
-            this.peleasGanadas = peleasGanadas;
-            // this.nombre = nombre;
-            // this.equipo = equipo;
-            // this.nombreReal = nombreReal;
-            // this.peleasGanadas = peleasGanadas;
-            // this.puedePelear = puedePelear;
-        }
-    }
-    let antman = new Avenger('Antman', 'Capi');
-    console.log(antman);
+(() => {
+    let sumar = (a, b) => {
+        return a + b;
+    };
 })();
