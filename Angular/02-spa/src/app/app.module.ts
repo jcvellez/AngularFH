a656fee62b03ec31d1b745f60cacc370be6ba283
jcvellez <jcvellez@live.com>
2022-11-26 18:18:02 -0500

    Me toca la clase 61 de la Seccion 4

--- Angular/02-spa/src/app/app.module.ts
+++ Angular/02-spa/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { APP_ROUTING } from './app.routes';
 
 //Services
-
+import { HeroesService } from "./services/heroes.service";
 
 //Components
 import { AppComponent } from './app.component';
@@ -13,6 +13,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
@@ -20,13 +21,17 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    HeroesService
+  ],
+  bootstrap: [
+    AppComponent]
 })
 export class AppModule { }
---
+++ Angular/02-spa/src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,19 @@
+import { Component} from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { HeroesService, Heroe } from "../../services/heroes.service";
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html'
+})
+export class HeroeComponent {
+  heroe: any = {};
+
+  constructor(private _activatedRoute:ActivatedRoute, _heroesService:HeroesService) { 
+  this._activatedRoute.params.subscribe(params=>{
+    this.heroe=_heroesService.getHeroe(params['id'])
+    console.log(this.heroe);
+    
+  });
+  }
+}
