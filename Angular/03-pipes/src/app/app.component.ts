abc4f8ee8f8bcf4e33a1d08e0077b37387fc27f2
vmt-jvelez <vmt-jvelez@bancoguayaquil.com>
2022-12-08 17:38:01 -0500

    Me toca clase 82 de la seccion 5

--- Angular/03-pipes/src/app/app.component.ts
+++ Angular/03-pipes/src/app/app.component.ts
@@ -9,4 +9,23 @@ export class AppComponent {
   nombre: string = "JuanCarlos";
   arreglo: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+
+  valorPromesa = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('llego la data..');
+    }, 4500);
+  });
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 21,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  };
+
 }
