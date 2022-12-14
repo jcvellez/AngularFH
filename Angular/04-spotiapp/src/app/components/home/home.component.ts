1d609e18c938d9e3cf754ce617507656b8d6fc22
vmt-jvelez <vmt-jvelez@bancoguayaquil.com>
2022-12-14 14:47:46 -0500

    Me toca la clase 96 de la Seccion 6 SpotiApp

--- Angular/04-spotiapp/src/app/components/home/home.component.ts
+++ Angular/04-spotiapp/src/app/components/home/home.component.ts
@@ -13,7 +13,7 @@ export class HomeComponent implements OnInit {
   constructor(private http: HttpClient) {
     console.log('constructor del home hecho');
 
-    this.http.get('https://restcountries.com/v3.1/name/united')
+    this.http.get('https://restcountries.com/v2/lang/es')
       .subscribe((resp: any )=> {
         this.paises = resp;
         console.log(resp);
